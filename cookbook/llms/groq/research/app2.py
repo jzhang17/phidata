21c7d61329a1d8f4bcd92cbc1c322383b53f57e1
Hongye (Jarvis) Zhang <zhy27027@gmail.com>
2024-07-09 07:05:40 -0700

    Update app2.py

--- cookbook/llms/groq/research/app2.py
+++ cookbook/llms/groq/research/app2.py
@@ -69,9 +69,6 @@ st.set_page_config(
     )
 
 st.title("JZ NewBizBot XL")
-
-st.warning('Upstream API data provider is experiencing issues right now. Search results are not accurate. This message will be removed when the issue is fixed. - JZ', icon="⚠️")
-
 # Add a dropdown to select the model
 model_option = st.selectbox(
     'Choose a model:',
